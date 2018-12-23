1a4eae77984aac2fd6badcdebe172970b8bbeff4
amagdi65 <45337019+amagdi65@users.noreply.github.com>
2018-12-23 19:48:33 +0200

    Add files via upload
    here i add my form classes

--- src/FormVerfication.java
+++ src/FormVerfication.java
@@ -1,3 +1,42 @@
 public class  FormVerfication {
-   
+    public Form f=new Form();
+    public FormDB FDB=new FormDB();
+
+    public boolean SaveData(Form ff)
+    {
+        IExceptions e = new IExceptions() ;
+        if(isTrue(ff))
+        {
+            FDB.AddFormRec(ff);
+            return true;
+        }
+        else
+        {
+            e.show_FormMsg();
+            return false;
+            //call it in exciption class
+        }
+    }
+    public boolean isTrue(Form ff)
+    {
+
+        IExceptions e = new IExceptions() ;
+        if(ff.getCategory()=="")
+        {
+            e.show_FormMsg();
+            return false;
+        }	// should the exception handel those cases
+        if(ff.getColor()=="")
+        {
+            e.show_FormMsg();
+            return false;
+        }
+
+        if(ff.getQuantity()=="")
+        {
+            e.show_FormMsg();
+            return false;
+        }
+        return true;
+    }
 }
